e2524caa9d8befef08dac2c16cd8cc7f231e9924
Johandro Gonzalez <88999789+JohandroG@users.noreply.github.com>
2021-11-16 16:28:55 -0600

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function prueba(event){
+
+    event.preventDefault();
+
+    let user = $('#user').val();
+    $.get(`https://api.github.com/users/${user}`, displayName)
+    console.log('Hola')
+    function displayName(data) {
+        console.log(data.login);
+        document.querySelector('.results').innerHTML =`
+        <h1>User: ${data.login}</h1>
+        <h2>Name: ${data.name}</h2>
+        <img style="width: 200px;" src = "${data.avatar_url}">
+        `;
+        
+    }
+}
+
+$( '.gitForm' ).on( 'submit', prueba )
 
